1d608cb1374ac8668542c3e41fbe1a916cb6bd79
fJinart <168510984+fJinart@users.noreply.github.com>
2024-05-15 10:09:04 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,14 +1,21 @@
-try{
-    window.addEventListener('load', function(){
-        setTimeout(scrollTo, 0,0,1);
+try {
+    window.addEventListener('load', function () {
+        setTimeout(scrollTo, 0, 0, 1);
     }, false);
-} catch(e){}
+} catch (e) { }
 
-$(function(){
-    $('.tab>li>a').click(function(){
+$(function () {
+    $('.tab>li>a').click(function () {
         $(this).parent().addClass("active")
-        .siblings()
-        .removeClass("active");
+            .siblings()
+            .removeClass("active");
         return false;
+    });
+});
+
+jQuery(document).ready(function () {
+
+    $(".menu_m").click(function () {
+        $(".popup_menu").slideToggle(500);
     });
 });
